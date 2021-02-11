1ceff73772c684a849738e6c404396b9ed83df4e
Polarpi <eugeniachen123@gmail.com>
2021-02-11 16:06:31 -0600

    add understrap theme

---
+++ wp-content/themes/understrap/home.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The home page file.
+ *
+ * @package understrap
+ */
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="index-wrapper">
+
+	<div id="content" tabindex="-1">
+
+
+	</div><!-- #content -->
+
+</div><!-- #index-wrapper -->
+
+<?php get_footer(); ?>
